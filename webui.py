1378a670b1a179ef8caa9232d30b9c45adbbaa0d
trongmin <99819934+trongminne@users.noreply.github.com>
2023-12-31 13:25:38 +0700

    a

--- webui.py
+++ webui.py
@@ -125,7 +125,7 @@ with shared.gradio_root:
 
                     # Gán giá trị của prompt bằng cách gọi hàm translate_text với giá trị nhập từ Textbox
                     prompt = translate_text(prompt_input.value)
-                    prompt = gr.Interface(prompt, parse_meta, inputs=[prompt, state_is_generating], outputs=[prompt, generate_button, load_parameter_button], queue=False, show_progress=False)
+                    prompt = gr.Interface(prompt, translate_text, inputs=[prompt, state_is_generating], outputs=[prompt, generate_button, load_parameter_button], queue=False, show_progress=False)
                     
                     default_prompt = modules.config.default_prompt
                     if isinstance(default_prompt, str) and default_prompt != '':
