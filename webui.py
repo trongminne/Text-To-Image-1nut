bf13ba796bf5583e8872fb0c159d4dbb019d6142
trongmin <99819934+trongminne@users.noreply.github.com>
2023-12-31 14:57:14 +0700

    a

--- webui.py
+++ webui.py
@@ -16,9 +16,8 @@ import modules.style_sorter as style_sorter
 import modules.meta_parser
 import args_manager
 import copy
-from translate import Translator
-
-translator = Translator(to_lang='en') # khởi tạo thư viện dịch thuật
+from googletrans import Translator
+translator = Translator() # khởi tạo thư viện dịch thuật
 from modules.sdxl_styles import legal_style_names
 from modules.private_logger import get_current_html_path
 from modules.ui_gradio_extensions import reload_javascript
@@ -117,7 +116,7 @@ with shared.gradio_root:
                     # Hàm xử lý sự kiện khi click vào nút "Tạo ảnh"
                     def handle_generate_button():
                         # Dịch văn bản từ tiếng Việt sang tiếng Anh
-                        translated_text = translator.translate(prompt.value)
+                        translated_text = translator.translate(prompt.value, src='vi', dest='en').text
                         # Cập nhật giá trị của prompt sau khi dịch xong
                         print(translated_text)
                         prompt.value = translated_text
