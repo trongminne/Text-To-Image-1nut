1ceb7dfadbdbb68f142b8ec2a51b7ae4f75fa64b
trongmin <99819934+trongminne@users.noreply.github.com>
2023-12-31 13:10:26 +0700

    a

--- webui.py
+++ webui.py
@@ -552,7 +552,7 @@ with shared.gradio_root:
 
             return json.dumps(loaded_json), gr.update(visible=False), gr.update(visible=True)
 
-        prompt_object.input(parse_meta, inputs=[prompt, state_is_generating], outputs=[prompt, generate_button, load_parameter_button], queue=False, show_progress=False)
+        prompt.input(parse_meta, inputs=[prompt, state_is_generating], outputs=[prompt, generate_button, load_parameter_button], queue=False, show_progress=False)
 
         load_parameter_button.click(modules.meta_parser.load_parameter_button_click, inputs=[prompt, state_is_generating], outputs=[
             advanced_checkbox,
