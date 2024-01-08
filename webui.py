a120da23b60bed2c50209e4cc966b704f8bb3cb6
trongmin <99819934+trongminne@users.noreply.github.com>
2024-01-08 17:05:00 +0700

    a

--- webui.py
+++ webui.py
@@ -120,13 +120,13 @@ with shared.gradio_root:
                 with gr.Column(scale=3, min_width=0):
                     generate_button = gr.Button(label="Tạo ảnh", value="Tạo ảnh", elem_classes='type_row', elem_id='generate_button', visible=True)
                     
-                    def generate_button_click_handler():
-                        # Gọi hàm dịch ngôn ngữ khi nút được nhấn
-                        prompt_vn = prompt.value
-                        prompt.value = translate_text(prompt_vn)
+                    # def generate_button_click_handler():
+                    #     # Gọi hàm dịch ngôn ngữ khi nút được nhấn
+                    #     prompt_vn = prompt.value
+                    #     prompt.value = translate_text(prompt_vn)
 
-                    # Tạo sự kiện cho nút generate_button
-                    generate_button.click(generate_button_click_handler)
+                    # # Tạo sự kiện cho nút generate_button
+                    # generate_button.click(generate_button_click_handler)
 
                     load_parameter_button = gr.Button(label="Cài dặt thông số", value="Tải thông số", elem_classes='type_row', elem_id='load_parameter_button', visible=False)
                     skip_button = gr.Button(label="Bỏ qua", value="Bỏ qua", elem_classes='type_row_half', visible=False)
