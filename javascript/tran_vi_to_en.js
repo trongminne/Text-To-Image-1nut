649d7cdafa701b690de5997202a660d110a830d1
trongmin <99819934+trongminne@users.noreply.github.com>
2024-01-08 14:14:56 +0700

    a

--- javascript/tran_vi_to_en.js
+++ javascript/tran_vi_to_en.js
@@ -1,20 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var generateButton = document.getElementById("generate_button");
 
     if (generateButton) {
-        generateButton.addEventListener("click", function () {
-            var vietnameseText = document.querySelector('.scroll-hide').value;
-            alert(vietnameseText);
-
-            // Kiểm tra xem các hàm có được định nghĩa đúng không
-            console.log("Calling translate_text...");
-            var englishText = translate_text(vietnameseText);
-            console.log("English Text:", englishText);
-
-            console.log("Calling generate_image...");
-            generate_image();
-        });
+        alert('lạndasjdg')
+        generateButton.click();
     } else {
         console.error("Element with id 'generate_button' not found.");
     }
+
 });
