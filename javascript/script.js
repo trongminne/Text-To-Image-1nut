514be724b8aa41564763e56e90df9c49a2926741
trongmin <99819934+trongminne@users.noreply.github.com>
2024-01-08 11:04:17 +0700

    a

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    var promptElement = document.getElementById('positive_prompt');
+    var promptElement = document.getElementsByClassName('scroll-hide');
     console.log('Prompt Element:', promptElement);
     if (promptElement) {
         alert('Đã truy cập vào trang web!');
