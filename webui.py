c5d0c30e3c77e375f0fcdbe3bb52537fa29519b0
trongmin <99819934+trongminne@users.noreply.github.com>
2024-01-08 20:30:26 +0700

    a

--- webui.py
+++ webui.py
@@ -122,7 +122,7 @@ with shared.gradio_root:
                     
                     def generate_button_click_handler():
                         # Gọi hàm dịch ngôn ngữ khi nút được nhấn
-                        prompt_vn = prompt.value
+                        prompt_vn = prompt.placeholder
                         print('prompt_vn: ', prompt.value)
                         # prompt.value = translate_text(prompt_vn)
                         # print('prompt_en: ', prompt.value)
