549020b46ea96adc391784d247cf3b61968ad63e
trongmin <99819934+trongminne@users.noreply.github.com>
2024-01-09 17:52:24 +0700

    a

--- modules/flags.py
+++ modules/flags.py
@@ -1,10 +1,10 @@
-disabled = 'Disabled'
-enabled = 'Enabled'
-subtle_variation = 'Vary (Subtle)'
-strong_variation = 'Vary (Strong)'
-upscale_15 = 'Upscale (1.5x)'
-upscale_2 = 'Upscale (2x)'
-upscale_fast = 'Upscale (Fast 2x)'
+disabled = 'Tắt'
+enabled = 'Bật'
+subtle_variation = 'Biến đổi nhẹ'
+strong_variation = 'Biến đổi mạnh'
+upscale_15 = 'Phóng lớn (1.5x)'
+upscale_2 = 'Phóng lớn (2x)'
+upscale_fast = 'Phóng lớn (Nhanh 2x)'
 
 uov_list = [
     disabled, subtle_variation, strong_variation, upscale_15, upscale_2, upscale_fast
@@ -14,14 +14,14 @@ KSAMPLER_NAMES = ["euler", "euler_ancestral", "heun", "heunpp2","dpm_2", "dpm_2_
                   "lms", "dpm_fast", "dpm_adaptive", "dpmpp_2s_ancestral", "dpmpp_sde", "dpmpp_sde_gpu",
                   "dpmpp_2m", "dpmpp_2m_sde", "dpmpp_2m_sde_gpu", "dpmpp_3m_sde", "dpmpp_3m_sde_gpu", "ddpm", "lcm"]
 
-SCHEDULER_NAMES = ["normal", "karras", "exponential", "sgm_uniform", "simple", "ddim_uniform", "lcm", "turbo"]
+SCHEDULER_NAMES = ["bình thường", "karras", "mũi tên số", "đồng đều sgm", "đơn giản", "ddim đồng đều", "lcm", "turbo"]
 SAMPLER_NAMES = KSAMPLER_NAMES + ["ddim", "uni_pc", "uni_pc_bh2"]
 
 sampler_list = SAMPLER_NAMES
 scheduler_list = SCHEDULER_NAMES
 
-cn_ip = "ImagePrompt"
-cn_ip_face = "FaceSwap"
+cn_ip = "Hình ảnh gợi ý"
+cn_ip_face = "Đổi khuôn mặt"
 cn_canny = "PyraCanny"
 cn_cpds = "CPDS"
 
@@ -30,15 +30,15 @@ default_ip = cn_ip
 
 default_parameters = {
     cn_ip: (0.5, 0.6), cn_ip_face: (0.9, 0.75), cn_canny: (0.5, 1.0), cn_cpds: (0.5, 1.0)
-}  # stop, weight
+}  # dừng, trọng số
 
-inpaint_engine_versions = ['None', 'v1', 'v2.5', 'v2.6']
-performance_selections = ['Speed', 'Quality', 'Extreme Speed']
+inpaint_engine_versions = ['Không có', 'v1', 'v2.5', 'v2.6']
+performance_selections = ['Tốc độ', 'Chất lượng', 'Tốc độ cực kỳ']
 
-inpaint_option_default = 'Inpaint or Outpaint (default)'
-inpaint_option_detail = 'Improve Detail (face, hand, eyes, etc.)'
-inpaint_option_modify = 'Modify Content (add objects, change background, etc.)'
+inpaint_option_default = 'Vẽ lại hoặc Xoá bỏ (mặc định)'
+inpaint_option_detail = 'Cải thiện chi tiết (khuôn mặt, tay, mắt, v.v.)'
+inpaint_option_modify = 'Chỉnh sửa Nội dung (thêm đối tượng, thay đổi nền, v.v.)'
 inpaint_options = [inpaint_option_default, inpaint_option_detail, inpaint_option_modify]
 
-desc_type_photo = 'Photograph'
-desc_type_anime = 'Art/Anime'
+desc_type_photo = 'Ảnh chụp'
+desc_type_anime = 'Nghệ thuật/Anime'
