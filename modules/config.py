f50ea91592b80dcacd2c20f33786a87be50a5db1
trongmin <99819934+trongminne@users.noreply.github.com>
2024-01-09 19:30:15 +0700

    a

--- modules/config.py
+++ modules/config.py
@@ -215,9 +215,9 @@ default_scheduler = get_config_item_or_set_default(
 default_styles = get_config_item_or_set_default(
     key='default_styles',
     default_value=[
-        "Tập trung V2",
-        "Tập trung Nâng cao",
-        "Tập trung Sắc nét"
+        "Fooocus V2",
+        "Fooocus Enhance",
+        "Fooocus Sharp"
     ],
     validator=lambda x: isinstance(x, list) and all(y in modules.sdxl_styles.legal_style_names for y in x)
 )
